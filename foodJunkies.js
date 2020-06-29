33409c3bb3f0260a72940c9d38b4afa6846437c7
Nicolas Lares <Nicolas.Lares29@gmail.com>
2020-06-29 11:43:04 -0700

    selectors added for cuisines

--- foodJunkies.js
+++ foodJunkies.js
@@ -1,17 +1,16 @@
-function buildQueryURL() {
+function cuisinesQuery() {
     var searchCity = $("#search-term").val();
-    console.log(searchCity);
     var queryURL = "https://developers.zomato.com/api/v2.1/cuisines?city_id=302";
     return queryURL;
 }
 function updatePage(zomatoData) {
     // Loop through and build elements for the defined number of articles
-
+    var searchSelect = $("#search-term")
     for (var i = 0; i < zomatoData.cuisines.length; i++) {
-
         console.log(zomatoData.cuisines[i]);
-        document.getElementById("article-section").innerHTML = JSON.stringify(zomatoData);
-
+        var cuisineId = zomatoData.cuisines[i].cuisine.cuisine_id;
+        var cuisineName = zomatoData.cuisines[i].cuisine.cuisine_name;
+        searchSelect.append("<option value='" + cuisineId + "'>" + cuisineName + "</option>");
     }
 }
 // Function to empty out the articles
@@ -23,7 +22,20 @@ $("#run-search").on("click", function (event) {
     event.preventDefault();
     // Empty the region associated with the articles
     clear();
-    var queryURL = buildQueryURL();
+    // var queryURL = cuisinesQuery();
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET",
+    //     headers: {
+    //         Accept: "application/json",
+    //         "user-key": "c6a0b0d5eee196a3d4c3ecebfde6ffcd"
+
+    //     }
+    // }).then(updatePage);
+});
+$("#clear-all").on("click", clear);
+
+var queryURL = cuisinesQuery();
     $.ajax({
         url: queryURL,
         method: "GET",
@@ -33,5 +45,3 @@ $("#run-search").on("click", function (event) {
 
         }
     }).then(updatePage);
-});
-$("#clear-all").on("click", clear);
